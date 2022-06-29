4426846e18bcd9be0b3f5a3133574818f8a2fc33
Piotr Baraniak <piotr.baraniak@gmail.com>
2022-06-29 13:07:06 +0200

    1 run

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+14 lines (12 sloc)  404 Bytes
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
